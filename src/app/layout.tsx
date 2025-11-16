d526df502680871597b9cf0da345d7695e52bd12
Stefano Taillefert <stefano.taille@gmail.com>
2025-11-16 21:50:34 +0100

    Add inital files

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Stefano Taillefert",
+  description: "My personal website",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <nav className="p-4 text-center">About me - Projects - Contact</nav>
+        <main className="p-4">{children}</main>
+      </body>
+    </html>
+  );
+}
